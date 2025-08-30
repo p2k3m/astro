130dd35a9f911e09ba30506d227cf2e7f80bfe49
Pushkar Mishra <55252939+p2k3m@users.noreply.github.com>
2025-08-30 11:50:44 +0530

    Reorder house polygon layout

--- tests/house-polygons.test.js
+++ tests/house-polygons.test.js
@@ -6,17 +6,17 @@ test('HOUSE_POLYGONS lists fixed paths for the twelve houses', () => {
   assert.strictEqual(HOUSE_POLYGONS.length, 12);
   const expected = [
     'M0 0 L0.5 0 L1 0 L1 0.5 L0.5 0.5 L0 0.5 Z',
-    'M1 0 L0.5 0.5 L0.5 0 Z',
-    'M1 0 L1 0.5 L0.5 0.5 Z',
-    'M1 0 L1 0.5 L1 1 L0.5 1 L0.5 0.5 L0.5 0 Z',
-    'M1 1 L0.5 0.5 L1 0.5 Z',
-    'M1 1 L0.5 1 L0.5 0.5 Z',
-    'M1 1 L0.5 1 L0 1 L0 0.5 L0.5 0.5 L1 0.5 Z',
-    'M0 1 L0.5 0.5 L0.5 1 Z',
-    'M0 1 L0 0.5 L0.5 0.5 Z',
-    'M0 1 L0 0.5 L0 0 L0.5 0 L0.5 0.5 L0.5 1 Z',
     'M0 0 L0.5 0.5 L0 0.5 Z',
     'M0 0 L0.5 0 L0.5 0.5 Z',
+    'M0 1 L0 0.5 L0 0 L0.5 0 L0.5 0.5 L0.5 1 Z',
+    'M0 1 L0.5 0.5 L0.5 1 Z',
+    'M0 1 L0 0.5 L0.5 0.5 Z',
+    'M1 1 L0.5 1 L0 1 L0 0.5 L0.5 0.5 L1 0.5 Z',
+    'M1 1 L0.5 0.5 L1 0.5 Z',
+    'M1 1 L0.5 1 L0.5 0.5 Z',
+    'M1 0 L1 0.5 L1 1 L0.5 1 L0.5 0.5 L0.5 0 Z',
+    'M1 0 L0.5 0.5 L0.5 0 Z',
+    'M1 0 L1 0.5 L0.5 0.5 Z',
   ];
   const pathFrom = (pts) =>
     pts.map(([x, y], i) => `${i === 0 ? 'M' : 'L'}${x} ${y}`).join(' ') + ' Z';
