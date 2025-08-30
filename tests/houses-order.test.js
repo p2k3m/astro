8a364fc2758a7627f1d0b9d72f51e7a2405e5c30
Pushkar Mishra <55252939+p2k3m@users.noreply.github.com>
2025-08-30 11:54:53 +0530

    Reverse house sign mapping (#101)

--- tests/houses-order.test.js
+++ tests/houses-order.test.js
@@ -8,9 +8,18 @@ test('computePositions produces houses in natural zodiac order', async () => {
   const data = await computePositions('2020-01-01T12:00+00:00', 0, 0);
   const k = data.ascSign;
   for (let h = 1; h <= 12; h++) {
-    assert.strictEqual(data.signInHouse[h], (k + h - 1) % 12);
+    assert.strictEqual(data.signInHouse[h], (k - (h - 1) + 12) % 12);
   }
-  assert.strictEqual(data.signInHouse[4], (data.signInHouse[1] + 3) % 12);
-  assert.strictEqual(data.signInHouse[7], (data.signInHouse[1] + 6) % 12);
-  assert.strictEqual(data.signInHouse[10], (data.signInHouse[1] + 9) % 12);
+  assert.strictEqual(
+    data.signInHouse[4],
+    (data.signInHouse[1] - 3 + 12) % 12
+  );
+  assert.strictEqual(
+    data.signInHouse[7],
+    (data.signInHouse[1] - 6 + 12) % 12
+  );
+  assert.strictEqual(
+    data.signInHouse[10],
+    (data.signInHouse[1] - 9 + 12) % 12
+  );
 });
