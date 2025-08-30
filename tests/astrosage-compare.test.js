19b08316c895f3eea90fb8c631f71e03ad4c6521
Pushkar Mishra <55252939+p2k3m@users.noreply.github.com>
2025-08-30 18:21:55 +0530

    Fix longitude sign and update house placements

--- tests/astrosage-compare.test.js
+++ tests/astrosage-compare.test.js
@@ -4,24 +4,24 @@ const { computePositions } = require('../src/lib/astro.js');
 
 test('Darbhanga 1982-12-01 03:50 matches AstroSage', async () => {
   const am = await computePositions('1982-12-01T03:50+05:30', 26.152, 85.897);
-  assert.strictEqual(am.ascSign, 0);
+  assert.strictEqual(am.ascSign, 6);
   const planets = Object.fromEntries(am.planets.map((p) => [p.name, p]));
   assert.deepStrictEqual(
-    am.planets.filter((p) => p.house === 1).map((p) => p.name),
+    am.planets.filter((p) => p.house === 7).map((p) => p.name),
     ['mercury', 'venus']
   );
-  assert.strictEqual(planets.sun.house, 8);
-  assert.strictEqual(planets.moon.house, 2);
-  assert.strictEqual(planets.jupiter.house, 8);
-  assert.strictEqual(planets.saturn.house, 7);
-});
-
-test('Darbhanga 1982-12-01 15:50 matches AstroSage', async () => {
-  const pm = await computePositions('1982-12-01T15:50+05:30', 26.152, 85.897);
-  assert.strictEqual(pm.ascSign, 6);
-  const planets = Object.fromEntries(pm.planets.map((p) => [p.name, p]));
   assert.strictEqual(planets.sun.house, 2);
   assert.strictEqual(planets.moon.house, 8);
   assert.strictEqual(planets.jupiter.house, 2);
   assert.strictEqual(planets.saturn.house, 1);
+});
+
+test('Darbhanga 1982-12-01 15:50 matches AstroSage', async () => {
+  const pm = await computePositions('1982-12-01T15:50+05:30', 26.152, 85.897);
+  assert.strictEqual(pm.ascSign, 1);
+  const planets = Object.fromEntries(pm.planets.map((p) => [p.name, p]));
+  assert.strictEqual(planets.sun.house, 7);
+  assert.strictEqual(planets.moon.house, 1);
+  assert.strictEqual(planets.jupiter.house, 7);
+  assert.strictEqual(planets.saturn.house, 6);
 });
