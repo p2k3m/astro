7a64f5ba9d1d595ffd7391101d3c986a74a752a8
Pushkar Mishra <55252939+p2k3m@users.noreply.github.com>
2025-08-30 17:53:38 +0530

    test: add Darbhanga July 1982 reference (#126)

---
+++ tests/darbhanga-july-1982.test.js
@@ -0,0 +1,10 @@
+const assert = require('node:assert');
+const test = require('node:test');
+const { computePositions } = require('../src/lib/astro.js');
+
+test('Darbhanga 1982-07-12 03:50 positions', async () => {
+  const res = await computePositions('1982-07-12T03:50+05:30', 26.15216, 85.89707);
+  assert.strictEqual(res.ascSign, 7);
+  const planets = Object.fromEntries(res.planets.map((p) => [p.name, p]));
+  assert.strictEqual(planets.moon.house, 4);
+});
