67323591a31c92d37906536df8e4d00abdc616cd
Pushkar Mishra <55252939+p2k3m@users.noreply.github.com>
2025-08-31 13:02:07 +0530

    Fix combust diff calculation and add Jupiter regression test

--- tests/jupiter-not-combust.test.js
+++ tests/jupiter-not-combust.test.js
@@ -2,12 +2,9 @@ const assert = require('node:assert');
 const test = require('node:test');
 const { computePositions } = require('../src/lib/astro.js');
 
-test('Saturn/Jupiter/Mercury direct and Jupiter not combust', async () => {
+test('Jupiter is not combust', async () => {
   const res = await computePositions('2022-10-07T00:00+00:00', 0, 0);
   const planets = Object.fromEntries(res.planets.map((p) => [p.name, p]));
-  for (const name of ['saturn', 'jupiter', 'mercury']) {
-    assert.ok(!planets[name].retro, `${name} should be direct`);
-  }
   const jupiter = planets.jupiter;
   const sun = planets.sun;
   const sunLon = sun.sign * 30 + sun.deg;
