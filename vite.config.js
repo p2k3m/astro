aff746ab35de6eb37dfe67ae02fec83d750964e4
Pushkar Mishra <55252939+p2k3m@users.noreply.github.com>
2025-09-01 16:20:40 +0530

    refactor: use local swisseph module (#226)

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
+  resolve: {
+    alias: {
+      'swisseph-v2': path.resolve(
+        path.dirname(new URL(import.meta.url).pathname),
+        './swisseph/index.js'
+      ),
+    },
+  },
   define: {
     __dirname: JSON.stringify(new URL('.', import.meta.url).pathname),
   },
